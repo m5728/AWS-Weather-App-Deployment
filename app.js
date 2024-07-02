811503b69401486deb55928b0a633a11162cab3d
m5728 <mattsp358@gmail.com>
2024-07-02 12:47:01 -0400

    mongo

--- app.js
+++ app.js
@@ -2,25 +2,42 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-const axios = require('axios')
-const bodyParser = require('body-parser')
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const API_KEY = process.env.API_KEY;  
+const mongoURI = process.env.MONGO_URI
 
 app.use(bodyParser.json());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/my-app/build')));
 
-// Endpoint for handling the weather request
 app.post('/getWeather', async (req, res) => {
   const { lat, long } = req.body;
   try {
     const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall/overview?lat=${lat}&lon=${long}&appid=${API_KEY}`);
+    
+    // Connect to MongoDB
+    const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    
+    const db = client.db('notetakingapp');
+    const collection = db.collection('notes');
+
+    await collection.insertOne({
+      latitude: lat,
+      longitude: long,
+      weather_data: response.data
+    });
+
+    client.close();
+    
     res.json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error fetching weather data' });
+    res.status(500).json({ error: 'Error fetching weather data or writing to database' });
   }
 });
 
